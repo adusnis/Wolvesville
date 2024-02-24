6a2db9dde8be6eeb2116b3c97683cccbad2711a7
Sinsuda <100611925+adusnis@users.noreply.github.com>
2024-02-25 03:52:09 +0700

    update

--- for research/role_cleaning.js
+++
@@ -1,8 +0,0 @@
-const roles = require('C:/Users/maewa/Programming/Wolvesville/wov/roleOnly.json');
-rolesList = []
-for (let i in roles) {
-    let role = roles[i].id
-    role = role.replace('-', '_')
-    rolesList.push(role)
-}
-console.log(rolesList)
